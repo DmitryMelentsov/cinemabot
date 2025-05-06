9425ef181f87d6ba0ac7dac952e0222af091a169
DmitryMelentsov <145838437+DmitryMelentsov@users.noreply.github.com>
2025-05-07 02:09:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 DB_PATH = os.getenv("DATABASE_URL")
 TOKEN_KINOPOISK = os.getenv("TOKEN_KINOPOISK")
 ZONA_URL = os.getenv("ZONA_URL")
-SECRET_KEY = "SECRET"###os.getenv("SECRET_KEY")###
+SECRET_KEY = os.getenv("SECRET_KEY")
 DOMAIN = "https://true-buckets-shop.loca.lt"
